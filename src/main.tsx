2a38f03a98a82284b2af3409232036ede06250f6
Diego Breskovit <81165451+dbreskovit@users.noreply.github.com>
2025-10-02 16:15:49 -0300

    feat: adding Mistica in the project

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import '@telefonica/mistica/css/mistica.css';
 import App from './App.tsx'
+import { getVivoNewSkin, ThemeContextProvider, type ThemeConfig } from '@telefonica/mistica'
+
+
+const theme: ThemeConfig = {
+  skin: getVivoNewSkin(),
+  colorScheme: 'light',
+  i18n: {
+    locale: 'pt-BR',
+    phoneNumberFormattingRegionCode: 'BR',
+  },
+};
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <ThemeContextProvider theme={theme}>
+      <App />
+    </ThemeContextProvider>
+  </StrictMode>
 )
